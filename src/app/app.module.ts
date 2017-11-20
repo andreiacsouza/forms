3f66e270c6101d7d5a59ead9446e736466867c7d
Andréia de Souza Ohe <andreiadesouzaohe@Andreias-MacBook-Air.local>
2017-11-19 16:40:23 -0800

    implemented controles ngForm, ngSubmit e ngModel

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -14,7 +16,9 @@ import { DataFormComponent } from './data-form/data-form.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpModule
   ],
   providers: [],
   bootstrap: [AppComponent]
