74cd2718a90b77d5a55b63dec33268313ed7b0c0
Andréia de Souza Ohe <andreiadesouzaohe@Andreias-MacBook-Air.local>
2017-12-20 18:45:13 -0800

    implemented form com codigo Angular

--- src/app/data-form/data-form.component.ts
+++ src/app/data-form/data-form.component.ts
@@ -1,5 +1,6 @@
+import { FormGroup, FormControl } from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
-import { FormGroup } from '@angular/forms';
+import { FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-data-form',
@@ -10,9 +11,19 @@ export class DataFormComponent implements OnInit {
 
   formulario: FormGroup;
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit() {
+
+    // this.formulario = new FormGroup({
+    //   nome: new FormControl(null), 
+    //   email: new FormControl(null)
+    // });
+
+    this.formulario = this.formBuilder.group({
+      nome: [null],
+      email: [null]
+    });
   }
 
 }
